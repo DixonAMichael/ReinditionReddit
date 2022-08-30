9c70fdb20319666e3546469b21d92bbade9175e3
rpeterson87 <oshorhino5053@gmail.com>
2022-08-30 16:10:08 -0400

    Test wokring???

--- navLinks.js
+++ navLinks.js
@@ -11,10 +11,9 @@ const routes = [
   
   module.exports = function navLinks(req, res, next) {
     if (req.session.currentUser) {
-      res.locals.routes = routes;
+        console.log(req.session)
+      return res.locals.routes = routes;
     } else {
-      res.locals.routes = authRoutes;
+      return res.locals.routes = authRoutes;
     }
-    // locals
-    next('this is not working');
   };
